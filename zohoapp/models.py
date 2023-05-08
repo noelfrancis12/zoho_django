82c7912e2f1c609819caa69db7ea6a1dc70c04d6
Nithya Vijayan <niyavijayan@gmail.com>
2023-05-08 19:03:34 +0530

    zoho-django 08-05-2023

--- zohoapp/models.py
+++ zohoapp/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class customer_details(models.Model):
+class company_details(models.Model):
+
     user = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
     contact_number = models.CharField(max_length=100,null=True,blank=True)
     company_name = models.CharField(max_length=100,null=True,blank=True)
     address = models.CharField(max_length=100,null=True,blank=True)
     city = models.CharField(max_length=100,null=True,blank=True)
     state = models.CharField(max_length=100,null=True,blank=True)
-    pincode = models.IntegerField(null=True,blank=True)
+    pincode = models.CharField(max_length = 100,null=True,blank=True)
     company_email = models.CharField(max_length=255,null=True,blank=True)
     business_name = models.CharField(max_length=255,null=True,blank=True)
     company_type = models.CharField(max_length=255,null=True,blank=True)
     industry_type = models.CharField(max_length=255,null=True,blank=True)
+    profile_pic = models.ImageField(null=True,blank = True,upload_to = 'image/profile')
+
--- zohoapp/views.py
+++ zohoapp/views.py
@@ -40,15 +40,14 @@ def register(request):
                 user.save()
                 u = User.objects.get(id = user.id)
 
-                customer_details(contact_number = phone, user = u).save()
-    
+                company_details(contact_number = phone, user = u).save()
         else:
             messages.info(request, 'Password doesnt match!!!!!!!')
             print("Password is not Matching.. ") 
             return redirect('register')   
         return redirect('register')
 
-    return render(request,'register.html')
+    return render(request,'register.html',{'msg' : messages})
 
 def login(request):
         
@@ -58,10 +57,10 @@ def login(request):
         password = request.POST['password']
 
         user = authenticate(request, username=email_or_username, password=password)
-        print(user)
+        # print(user)
         if user is not None:
             auth.login(request, user)
-            return redirect('home')
+            return redirect('base')
         else:
             return redirect('/')
 
@@ -73,5 +72,53 @@ def logout(request):
     return redirect('/')
 
 @login_required(login_url='login')
-def home(request):
-    return render(request,'home.html')
+def base(request):
+
+    company = company_details.objects.get(user = request.user)
+    context = {
+                'company' : company
+            }
+    return render(request,'base.html',context)
+
+@login_required(login_url='login')
+def view_profile(request):
+
+    company = company_details.objects.get(user = request.user)
+    context = {
+                'company' : company
+            }
+    return render(request,'profile.html',context)
+
+@login_required(login_url='login')
+def edit_profile(request,pk):
+
+    company = company_details.objects.get(id = pk)
+    user1 = User.objects.get(id = company.user_id)
+
+    if request.method == "POST":
+
+        user1.first_name = capfirst(request.POST.get('f_name'))
+        user1.last_name  = capfirst(request.POST.get('l_name'))
+        user1.email = request.POST.get('email')
+        company.contact_number = request.POST.get('cnum')
+        company.address = capfirst(request.POST.get('ards'))
+        company.company_name = request.POST.get('comp_name')
+        company.company_email = request.POST.get('comp_email')
+        company.city = request.POST.get('city')
+        company.state = request.POST.get('state')
+        company.pincode = request.POST.get('pinc')
+        company.business_name = request.POST.get('bname')
+        company.company_type = request.POST.get('comp_type')
+        if len(request.FILES)!=0 :
+            company.profile_pic = request.FILES.get('file')
+
+        company.save()
+        user1.save()
+        return redirect('view_profile')
+
+    context = {
+        'company' : company,
+        'user1' : user1,
+    }
+    
+    return render(request,'edit_profile.html',context)
