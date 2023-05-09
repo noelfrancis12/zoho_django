586de23371b914bcd36ef75635bf904409d6c000
Nithya Vijayan <niyavijayan@gmail.com>
2023-05-09 18:15:02 +0530

    zho-django 09-05-2023

--- zohoapp/urls.py
+++ zohoapp/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path,include
 from.import views
+from django.urls import path,include,re_path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.static import serve
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -9,4 +13,36 @@ urlpatterns = [
     path('logout', views.logout, name='logout'),
     path('view_profile', views.view_profile, name='view_profile'),
     path('edit_profile/<pk>', views.edit_profile, name='edit_profile'),
+    path('itemview',views.itemview,name='itemview'),
+    path('additem',views.additem,name='additem'),
+
+    path('add',views.add,name='add'),
+    path('add_account',views.add_account,name='add_account'),
+    path('detail/<int:id>',views.detail,name='detail'),
+    path('edititem/<int:id>',views.edititem,name='edititem'),
+    path('edit_db/<int:id>',views.edit_db,name='edit_db'),
+    path('Action/<int:id>',views.Action,name='Action'),
+    path('cleer/<int:id>',views.cleer,name='cleer'),
+    path('add_unit',views.add_unit,name='add_unit'),
+    path('sales',views.add_sales,name='add_sales'),
+    path('vendor/',views.vendor,name='vendor'),
+    path('add_vendor/',views.add_vendor,name='add_vendor'),
+    path('sample/',views.sample,name="sample"),
+    path('view_vendor_list/',views.view_vendor_list,name='view_vendor_list'),
+    path('view_vendor_details/<int:pk>',views.view_vendor_details,name='view_vendor_details'),
+    path('add_comment/<int:pk>',views.add_comment,name='add_comment'),
+    path('sendmail/<int:pk>',views.sendmail,name='sendmail'),
+    path('edit_vendor/<int:pk>',views.edit_vendor,name='edit_vendor'),
+    path('edit_vendor_details/<int:pk>',views.edit_vendor_details,name='edit_vendor_details'),
+    path('upload_document/<int:pk>',views.upload_document,name='upload_document'),
+    path('download_doc/<int:pk>',views.download_doc,name='download_doc'),
+    path('cancel_vendor/',views.cancel_vendor,name='cancel_vendor'),
+    path('delete_vendor/<int:pk>',views.delete_vendor,name='delete_vendor'),
+    
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    
+
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- zohoapp/views.py
+++ zohoapp/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib import messages
 from django.utils.text import capfirst
 from django.contrib.auth.models import User,auth
 from .models import *
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
-
+from django.http import HttpResponse
+from django.db.models import Q
+from django.conf import settings
 
 
 
@@ -122,3 +124,401 @@ def edit_profile(request,pk):
     }
     
     return render(request,'edit_profile.html',context)
+
+
+@login_required(login_url='login')
+def itemview(request):
+    viewitem=AddItem.objects.all()
+    return render(request,'item_view.html',{'view':viewitem})
+
+@login_required(login_url='login')
+def additem(request):
+    unit=Unit.objects.all()
+    sale=Sales.objects.all()
+    purchase=Purchase.objects.all()
+
+    accounts = Purchase.objects.all()
+    account_types = set(Purchase.objects.values_list('Account_type', flat=True))
+
+    
+    account = Sales.objects.all()
+    account_type = set(Sales.objects.values_list('Account_type', flat=True))
+    
+    
+
+    return render(request,'additem.html',{'unit':unit,'sale':sale,'purchase':purchase,
+               
+                            "account":account,"account_type":account_type,"accounts":accounts,"account_types":account_types,
+                            
+                            })
+
+@login_required(login_url='login')
+def add_account(request):
+    if request.method=='POST':
+        Account_type  =request.POST['acc_type']
+        Account_name =request.POST['acc_name']
+        Acount_code =request.POST['acc_code']
+        Account_desc =request.POST['acc_desc']
+       
+        acc=Purchase(Account_type=Account_type,Account_name=Account_name,Acount_code=Acount_code,Account_desc=Account_desc)
+        acc.save()                 
+        return redirect("additem")
+        
+    return render(request,'additem.html')
+
+
+@login_required(login_url='login')
+def add(request):
+    if request.user.is_authenticated:
+        if request.method=='POST':
+            radio=request.POST.get('radio')
+            if radio=='tax':
+    
+                
+                inter=request.POST['inter']
+                intra=request.POST['intra']
+                type=request.POST.get('type')
+                name=request.POST['name']
+                unit=request.POST['unit']
+                sel_price=request.POST.get('sel_price')
+                sel_acc=request.POST.get('sel_acc')
+                s_desc=request.POST.get('sel_desc')
+                cost_price=request.POST.get('cost_price')
+                cost_acc=request.POST.get('cost_acc')      
+                p_desc=request.POST.get('cost_desc')
+                u=request.user.id
+                us=request.user
+                history="Created by" + str(us)
+                user=User.objects.get(id=u)
+                unit=Unit.objects.get(id=unit)
+                sel=Sales.objects.get(id=sel_acc)
+                cost=Purchase.objects.get(id=cost_acc)
+                ad_item=AddItem(type=type,Name=name,p_desc=p_desc,s_desc=s_desc,s_price=sel_price,p_price=cost_price,unit=unit,
+                            sales=sel,purchase=cost,user=user,creat=history,interstate=inter,intrastate=intra
+                                )
+                
+            else:
+                                                  
+                type=request.POST.get('type')
+                name=request.POST['name']
+                unit=request.POST['unit']
+                sel_price=request.POST.get('sel_price')
+                sel_acc=request.POST.get('sel_acc')
+                s_desc=request.POST.get('sel_desc')
+                cost_price=request.POST.get('cost_price')
+                cost_acc=request.POST.get('cost_acc')      
+                p_desc=request.POST.get('cost_desc')
+                u=request.user.id
+                us=request.user
+                history="Created by" + str(us)
+                user=User.objects.get(id=u)
+                unit=Unit.objects.get(id=unit)
+                sel=Sales.objects.get(id=sel_acc)
+                cost=Purchase.objects.get(id=cost_acc)
+                ad_item=AddItem(type=type,Name=name,p_desc=p_desc,s_desc=s_desc,s_price=sel_price,p_price=cost_price,unit=unit,
+                            sales=sel,purchase=cost,user=user,creat=history,interstate='none',intrastate='none'
+                                )
+                ad_item.save()
+            ad_item.save()
+           
+            return redirect("itemview")
+    return render(request,'additem.html')
+
+
+@login_required(login_url='login')
+def edititem(request,id):
+    pedit=AddItem.objects.get(id=id)
+    p=Purchase.objects.all()
+    s=Sales.objects.all()
+    u=Unit.objects.all()
+
+    accounts = Purchase.objects.all()
+    account_types = set(Purchase.objects.values_list('Account_type', flat=True))
+    
+
+    
+    account = Sales.objects.all()
+    account_type = set(Sales.objects.values_list('Account_type', flat=True))
+    
+    return render(request,'edititem.html',{"account":account,"account_type":account_type,'e':pedit,'p':p,'s':s,'u':u,"accounts":accounts,"account_types":account_types})
+
+
+@login_required(login_url='login')
+def edit_db(request,id):
+        if request.method=='POST':
+            edit=AddItem.objects.get(id=id)
+            edit.type=request.POST.get('type')
+            edit.Name=request.POST['name']
+            unit=request.POST['unit']
+            edit.s_price=request.POST['sel_price']
+            sel_acc=request.POST['sel_acc']
+            edit.s_desc=request.POST['sel_desc']
+            edit.p_price=request.POST['cost_price']
+            cost_acc=request.POST['cost_acc']        
+            edit.p_desc=request.POST['cost_desc']
+            
+            
+            edit.unit=Unit.objects.get(id=unit)
+            edit.sales=Sales.objects.get(id=sel_acc)
+            edit.purchase=Purchase.objects.get(id=cost_acc)
+            edit.save()
+            return redirect('itemview')
+
+        return render(request,'edititem.html')
+
+
+@login_required(login_url='login')
+def detail(request,id):
+    user_id=request.user
+    items=AddItem.objects.all()
+    product=AddItem.objects.get(id=id)
+    history=History.objects.filter(p_id=product.id)
+    print(product.id)
+    
+    
+    context={
+       "allproduct":items,
+       "product":product,
+       "history":history,
+      
+    }
+    
+    return render(request,'demo.html',context)
+
+
+@login_required(login_url='login')
+def Action(request,id):
+    user=request.user.id
+    user=User.objects.get(id=user)
+    viewitem=AddItem.objects.all()
+    event=AddItem.objects.get(id=id)
+    
+
+    print(user)
+    if request.method=='POST':
+        action=request.POST['action']
+        event.satus=action
+        event.save()
+        if action == 'active':
+            History(user=user,message="Item marked as Active ",p=event).save()
+        else:
+            History(user=user,message="Item marked as inActive",p=event).save()
+    return render(request,'item_view.html',{'view':viewitem})
+
+@login_required(login_url='login')
+def cleer(request,id):
+    dl=AddItem.objects.get(id=id)
+    dl.delete()
+    return redirect('itemview')
+
+
+@login_required(login_url='login')
+def add_unit(request):
+    if request.method=='POST':
+        unit_name=request.POST['unit_name']
+        Unit(unit=unit_name).save()
+        return redirect('additem')
+    return render(request,"additem.html")
+
+
+@login_required(login_url='login')
+def add_sales(request):
+    if request.method=='POST':
+        Account_type  =request.POST['acc_type']
+        Account_name =request.POST['acc_name']
+        Acount_code =request.POST['acc_code']
+        Account_desc =request.POST['acc_desc']        
+        acc=Sales(Account_type=Account_type,Account_name=Account_name,Acount_code=Acount_code,Account_desc=Account_desc)
+        acc.save()
+        return redirect('additem')
+    return render(request,'additem.html')
+
+
+@login_required(login_url='login')
+def vendor(request):
+    return render(request,'create_vendor.html')
+
+
+
+@login_required(login_url='login')
+def add_vendor(request):
+    if request.method=="POST":
+        vendor_data=vendor_table()
+        vendor_data.salutation=request.POST['salutation']
+        vendor_data.first_name=request.POST['first_name']
+        vendor_data.last_name=request.POST['last_name']
+        vendor_data.company_name=request.POST['company_name']
+        vendor_data.vendor_display_name=request.POST['v_display_name']
+        vendor_data.vendor_email=request.POST['vendor_email']
+        vendor_data.vendor_wphone=request.POST['w_phone']
+        vendor_data.vendor_mphone=request.POST['m_phone']
+        vendor_data.skype_number=request.POST['skype_number']
+        vendor_data.designation=request.POST['designation']
+        vendor_data.department=request.POST['department']
+        vendor_data.website=request.POST['website']
+        vendor_data.gst_treatment=request.POST['gst']
+
+        x=request.POST['gst']
+        if x=="Unregistered Business-not Registered under GST":
+            vendor_data.pan_number=request.POST['pan_number']
+            vendor_data.gst_number="null"
+        else:
+            vendor_data.gst_number=request.POST['gst_number']
+            vendor_data.pan_number=request.POST['pan_number']
+
+        vendor_data.source_supply=request.POST['source_supply']
+        vendor_data.currency=request.POST['currency']
+        vendor_data.opening_bal=request.POST['opening_bal']
+        vendor_data.payment_terms=request.POST['payment_terms']
+
+        user_id=request.user.id
+        udata=User.objects.get(id=user_id)
+        vendor_data.user=udata
+
+        
+
+
+
+        
+        # vendor_data=vendor(user=udata,salutation=salutation,first_name=first_name,last_name=last_name,
+        #     company_name=company_name,vendor_display_name=vendor_display_name,vendor_email=vendor_email,
+        #     vendor_wphone=vendor_wphone,vendor_mphone=vendor_mphone,skype_number=skype_number,designation=designation,
+        #     department=department,website=website,gst_treatment=gst_treatment,source_supply=source_supply,
+        #     gst_number=gst_number,pan_number=pan_number,currency=currency,opening_bal=opening_bal,
+        #     payment_terms=payment_terms)
+        
+        
+        
+        
+        vendor_data.save()
+        return redirect('base')
+        
+@login_required(login_url='login')
+def sample(request):
+    print("hello")
+    return redirect('base')
+
+@login_required(login_url='login')
+def view_vendor_list(request):
+    user_id=request.user.id
+    udata=User.objects.get(id=user_id)
+    data=vendor_table.objects.filter(user=udata)
+    return render(request,'vendor_list.html',{'data':data})
+
+@login_required(login_url='login')
+def view_vendor_details(request,pk):
+    user_id=request.user.id
+    udata=User.objects.get(id=user_id)
+    vdata1=vendor_table.objects.filter(user=udata)
+    vdata2=vendor_table.objects.get(id=pk)
+    mdata=mail_table.objects.filter(vendor=vdata2)
+    ddata=doc_upload_table.objects.filter(user=udata,vendor=vdata2)
+
+    return render(request,'vendor_details.html',{'vdata':vdata1,'vdata2':vdata2,'mdata':mdata,'ddata':ddata})
+
+@login_required(login_url='login')
+def add_comment(request,pk):
+    if request.method=='POST':
+        comment=request.POST['comment']
+        user_id=request.user.id
+        udata=User.objects.get(id=user_id)
+        vdata2=vendor_table.objects.get(id=pk)
+        comments=comments_table(user=udata,vendor=vdata2,comment=comment)
+        comments.save()
+        return redirect("view_vendor_list")
+
+@login_required(login_url='login')
+def sendmail(request,pk):
+    if request.method=='POST':
+        user_id=request.user.id
+        udata=User.objects.get(id=user_id)
+        vdata2=vendor_table.objects.get(id=pk)
+        mail_from=settings.EMAIL_HOST_USER
+        mail_to=request.POST['email']
+        subject=request.POST['subject']
+        content=request.POST['content']
+        mail_data=mail_table(user=udata,vendor=vdata2,mail_from=mail_from,mail_to=mail_to,subject=subject,content=content)
+        mail_data.save()
+
+        subject = request.POST['subject']
+        message = request.POST['content']
+        recipient = request.POST['email']     #  recipient =request.POST["inputTagName"]
+        send_mail(subject, message, settings.EMAIL_HOST_USER, [recipient])
+
+        return redirect("view_vendor_list")
+
+
+@login_required(login_url='login')
+def edit_vendor(request,pk):
+    vdata=vendor_table.objects.get(id=pk)
+    return render(request,'edit_vendor.html',{'vdata':vdata})
+
+@login_required(login_url='login')
+def edit_vendor_details(request,pk):
+    if request.method=='POST':
+        vdata=vendor_table.objects.get(id=pk)
+        vdata.salutation=request.POST['salutation']
+        vdata.first_name=request.POST['first_name']
+        vdata.last_name=request.POST['last_name']
+        vdata.company_name=request.POST['company_name']
+        vdata.vendor_display_name=request.POST['v_display_name']
+        vdata.vendor_email=request.POST['vendor_email']
+        vdata.vendor_wphone=request.POST['w_phone']
+        vdata.vendor_mphone=request.POST['m_phone']
+        vdata.skype_number=request.POST['skype_number']
+        vdata.designation=request.POST['designation']
+        vdata.department=request.POST['department']
+        vdata.website=request.POST['website']
+        vdata.gst_treatment=request.POST['gst']
+        if vdata.gst_treatment=="Unregistered Business-not Registered under GST":
+            vdata.pan_number=request.POST['pan_number']
+            vdata.gst_number="null"
+        else:
+            vdata.gst_number=request.POST['gst_number']
+            vdata.pan_number=request.POST['pan_number']
+
+        vdata.source_supply=request.POST['source_supply']
+        vdata.currency=request.POST['currency']
+        vdata.opening_bal=request.POST['opening_bal']
+        vdata.payment_terms=request.POST['payment_terms']
+
+        vdata.save()
+        return redirect("view_vendor_list")
+
+@login_required(login_url='login')
+def upload_document(request,pk):
+    if request.method=='POST':
+        user_id=request.user.id
+        udata=User.objects.get(id=user_id)
+        vdata=vendor_table.objects.get(id=pk)
+        title=request.POST['title']
+        document=request.FILES.get('file')
+        doc_data=doc_upload_table(user=udata,vendor=vdata,title=title,document=document)
+        doc_data.save()
+        return redirect("view_vendor_list")
+
+@login_required(login_url='login')
+def download_doc(request,pk):
+    document=get_object_or_404(doc_upload_table,id=pk)
+    response=HttpResponse(document.document,content_type='application/pdf')
+    response['Content-Disposition']=f'attachment; filename="{document.document.name}"'
+    return response
+
+@login_required(login_url='login')
+def cancel_vendor(request):
+    return redirect("view_vendor_list")
+
+@login_required(login_url='login')
+def delete_vendor(request,pk):
+    if comments_table.objects.filter(vendor=pk).exists():
+        user2=comments_table.objects.filter(vendor=pk)
+        user2.delete()
+    if mail_table.objects.filter(vendor=pk).exists():
+        user3=mail_table.objects.filter(vendor=pk)
+        user3.delete()
+    if doc_upload_table.objects.filter(vendor=pk).exists():
+        user4=doc_upload_table.objects.filter(vendor=pk)
+        user4.delete()
+    user1=vendor_table.objects.get(id=pk)
+    user1.delete()
+    return redirect("view_vendor_list")
