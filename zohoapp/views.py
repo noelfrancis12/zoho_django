642e968c1e4b218db96cd05ca28213d8d9dd211f
Nithya Vijayan <niyavijayan@gmail.com>
2023-05-24 23:22:57 +0530

    zoho django 24-05-2023

--- zohoapp/views.py
+++ zohoapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib import messages
 from django.utils.text import capfirst
 from django.contrib.auth.models import User,auth
